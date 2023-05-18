6698d5eb6efcbdf81400899088d4759d9bdcd960
sunny <rosa7536@naver.com>
2023-05-18 23:02:05 +0900

    CHORE: 불필요한 import 제거

--- server/src/main/java/sopt/mysoptin/server/service/RetrospectService.java
+++ server/src/main/java/sopt/mysoptin/server/service/RetrospectService.java
@@ -1,6 +1,5 @@
 package sopt.mysoptin.server.service;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,7 +8,6 @@ import sopt.mysoptin.server.domain.Retrospect;
 import sopt.mysoptin.server.infrastructure.RetrospectRepository;
 
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 
 @Service
 @RequiredArgsConstructor
