e5ebbcd5615fcdce47305a95ab7ebd3fc7396597
yummygyudon <bang2brew@gmail.com>
2023-05-19 05:59:44 +0900

    FEAT : 공통 사용 Class 및 Enum 코드 통일화
    - ($common/dto) ApiResponse
    - ($exception) Error
    - ($exception) Success
    - ($controller/dto/response) RetrospectResponseDto

---
+++ server/src/main/java/sopt/mysoptin/server/controller/dto/response/RetrospectResponseDto.java
@@ -0,0 +1,22 @@
+package sopt.mysoptin.server.controller.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RetrospectResponseDto {
+    private Long retrospectId;
+    private String descRoutine;
+    private String descBest;
+    private String descSelf;
+    private boolean isPublic;
+    private String writtenDate;
+
+    public static RetrospectResponseDto of(Long retrospectId, String descRoutine, String descBest, String descSelf, boolean isPublic, String writtenDate) {
+        return new RetrospectResponseDto(retrospectId, descRoutine, descBest, descSelf, isPublic, writtenDate);
+    }
+}
