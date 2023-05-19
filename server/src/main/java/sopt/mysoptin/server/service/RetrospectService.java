889640362fa0af5b79e31838166cedc228bcc8e1
yummygyudon <bang2brew@gmail.com>
2023-05-19 14:21:00 +0900

    FEAT : Retrospect 수정 Service 메서드 정의
    - Request Body 데이터 Converting
    - 수정 결과를 Response Body 에 담길 RetrospectResponseDto로 Converting

--- server/src/main/java/sopt/mysoptin/server/service/RetrospectService.java
+++ server/src/main/java/sopt/mysoptin/server/service/RetrospectService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sopt.mysoptin.server.controller.dto.request.RetrospectRequestCreateDto;
+import sopt.mysoptin.server.controller.dto.request.RetrospectRequestUpdateDto;
 import sopt.mysoptin.server.controller.dto.response.RetrospectResponseDto;
 import sopt.mysoptin.server.domain.Retrospect;
 import sopt.mysoptin.server.infrastructure.RetrospectRepository;
@@ -36,6 +37,24 @@ public class RetrospectService {
                 createdRetrospect.getDescSelf(),
                 createdRetrospect.isPublic(),
                 createdRetrospect.getWrittenDate().toString()
+        );
+    }
+
+    @Transactional
+    public RetrospectResponseDto updateRetrospect(Long targetId, RetrospectRequestUpdateDto updateDto){
+        retrospectRepository.updateDescRoutine(targetId, updateDto.getDescRoutine());
+        retrospectRepository.updateDescBest(targetId, updateDto.getDescBest());
+        retrospectRepository.updateDescSelf(targetId, updateDto.getDescSelf());
+        retrospectRepository.updateIsPublic(targetId, updateDto.isPublic());
+
+        Retrospect updatedRetrospect = retrospectRepository.findByRetrospectId(targetId);
+        return RetrospectResponseDto.of(
+                updatedRetrospect.getRetrospectId(),
+                updatedRetrospect.getDescRoutine(),
+                updatedRetrospect.getDescBest(),
+                updatedRetrospect.getDescSelf(),
+                updatedRetrospect.isPublic(),
+                updatedRetrospect.getWrittenDate().toString()
         );
     }
 
