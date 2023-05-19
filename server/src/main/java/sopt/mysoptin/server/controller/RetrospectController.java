b0b372ae4108f276c24f10fa6dfe471b2bd7fe8b
yummygyudon <bang2brew@gmail.com>
2023-05-19 09:25:02 +0900

    FEAT : 신규 Retrospect 추가 POST Reqeust 매핑 Controller 정의
    - Status : 201 CREATED
    - 저장 결과 반환

---
+++ server/src/main/java/sopt/mysoptin/server/controller/RetrospectController.java
@@ -0,0 +1,27 @@
+package sopt.mysoptin.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import sopt.mysoptin.server.common.dto.ApiResponse;
+import sopt.mysoptin.server.controller.dto.request.RetrospectRequestCreateDto;
+import sopt.mysoptin.server.controller.dto.response.RetrospectResponseDto;
+import sopt.mysoptin.server.exception.Success;
+import sopt.mysoptin.server.service.RetrospectService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/retro")
+@Slf4j
+public class RetrospectController {
+    private final RetrospectService retrospectService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<RetrospectResponseDto> create(@RequestBody final RetrospectRequestCreateDto requestCreateDto) {
+        log.info(Boolean.toString(requestCreateDto.isPublic()));
+        return ApiResponse.success(Success.POST_RETROSPECT_SUCCESS, retrospectService.createRetrospect(requestCreateDto));
+    }
+
+}
