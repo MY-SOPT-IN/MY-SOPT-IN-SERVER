c86aa6fd1404f51e57cc29ed858e86cb870d294d
yummygyudon <bang2brew@gmail.com>
2023-05-19 09:15:48 +0900

    FEAT : POST 전용 Request Data 매핑 DTO 정의
    - id 없는 상태의 Request에 맞춘 DTO
    - JSON 속성명 Mapping 오류 @JsonProperty 어노테이션으로 해결

---
+++ server/src/main/java/sopt/mysoptin/server/controller/dto/request/RetrospectRequestCreateDto.java
@@ -0,0 +1,21 @@
+
+package sopt.mysoptin.server.controller.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RetrospectRequestCreateDto {
+    @JsonProperty("isPublic")
+    private boolean isPublic;
+    private String descRoutine;
+    private String descBest;
+    private String descSelf;
+    private String writtenDate;
+
+}
