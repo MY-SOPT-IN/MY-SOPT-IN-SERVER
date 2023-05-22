8a7114e921d8aaa57b98e35d17d8e082cefc1374
sunny <rosa7536@naver.com>
2023-05-22 23:15:42 +0900

    FEAT : Routine, Repeat 엔티티 추가

---
+++ server/src/main/java/sopt/mysoptin/server/infrastructure/RoutineRepository.java
@@ -0,0 +1,14 @@
+package sopt.mysoptin.server.infrastructure;
+
+import org.springframework.data.repository.Repository;
+import sopt.mysoptin.server.domain.Routine;
+
+public interface RoutineRepository extends Repository<Routine, Long> {
+    // CREATE
+
+    // READ
+
+    // UPDATE
+
+    // DELETE
+}
