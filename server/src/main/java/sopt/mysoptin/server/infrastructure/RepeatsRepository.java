617c04ed3f933600a9b0776aa3999f3f0adf61c0
sunny <rosa7536@naver.com>
2023-05-23 16:15:47 +0900

    FIX : JPARepository로 변경

--- server/src/main/java/sopt/mysoptin/server/infrastructure/RepeatsRepository.java
+++ server/src/main/java/sopt/mysoptin/server/infrastructure/RepeatsRepository.java
@@ -1,9 +1,10 @@
 package sopt.mysoptin.server.infrastructure;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 import sopt.mysoptin.server.domain.Repeats;
 
-public interface RepeatsRepository extends Repository<Repeats, Long> {
+public interface RepeatsRepository extends JpaRepository<Repeats, Long> {
     // CREATE
 
     // READ
--- server/src/main/java/sopt/mysoptin/server/infrastructure/RoutineRepository.java
+++ server/src/main/java/sopt/mysoptin/server/infrastructure/RoutineRepository.java
@@ -1,9 +1,10 @@
 package sopt.mysoptin.server.infrastructure;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 import sopt.mysoptin.server.domain.Routine;
 
-public interface RoutineRepository extends Repository<Routine, Long> {
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
     // CREATE
 
     // READ
